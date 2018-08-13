c4842a43819d9fa3733fc56c9d322d3518ce020a
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-08-13 13:33:49 -0500

    added delete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+SELECT album FROM albums WHERE release_date > 1991;
+
+SELECT album FROM albums WHERE genre = '%disco%';
+
+SELECT album FROM albums WHERE artist = 'Whitney Houston';
